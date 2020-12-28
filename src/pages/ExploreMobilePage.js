6d7cda84bb1f48d52a31d5eaa9d6786b74e37074
Issamoxix <issamhaidaoui@gmail.com>
2020-12-28 19:46:52 +0100

    another commit

--- src/pages/ExploreMobilePage.js
+++ src/pages/ExploreMobilePage.js
@@ -2,13 +2,26 @@ import React from "react";
 import "../styles/ExploreMobilePageStyles.css";
 import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
 import SearchCard from "../components/SearchCard";
+import ytb from "../assets/youtube.png";
+import sdc from "../assets/soundcloud.png";
+import { selectPlat, setPlat } from "../features/app/appSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const ExploreMobilePage = () => {
+  const plat = useSelector(selectPlat);
+  const dispatch = useDispatch();
   return (
     <div className="MobileExploreContainer">
       <div className="Header">
         <h2>RemotePlayer</h2>
-        <h2 className="TogglePlayMobile">X</h2>
+        <img
+          className="TogglePlayMobile"
+          src={plat === "sdc" ? sdc : ytb}
+          alt="plat"
+          onClick={() =>
+            dispatch(setPlat({ plat: plat === "sdc" ? "ytb" : "sdc" }))
+          }
+        />
         <form className="SearchForm">
           <input type="text" className="SearchInput" placeholder="Search" />
           <SearchRoundedIcon
